095579bedb70993d13982d94231989495788e8ee
Meryembarkallah21 <meryembarkallah21@gmail.com>
2023-11-26 17:33:26 +0100

    Initial commit

---
+++ src/test/java/com/meryembarkallah21/gamehub/GameHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meryembarkallah21.gamehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
